686589f9e5e420b9b2ca3407d730290c9bff5a4e
ajaysingh132 <banjarajanmat@gmail.com>
2025-12-15 07:56:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/gbsb4u/webbrowser/MainActivity.java
+++ app/src/main/java/com/gbsb4u/webbrowser/MainActivity.java
@@ -1,1 +1,42 @@
+package com.gbsb4u.webbrowser;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setUserAgentString(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/120.0 Safari/537.36"
+        );
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://gbsb.in");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
