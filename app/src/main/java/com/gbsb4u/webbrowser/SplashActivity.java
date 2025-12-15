8fddd390985e7aef836f597ca54dbf05b4c60bb1
ajaysingh132 <banjarajanmat@gmail.com>
2025-12-15 08:00:07 +0530

    Update SplashActivity.java

--- app/src/main/java/com/gbsb4u/webbrowser/SplashActivity.java
+++ app/src/main/java/com/gbsb4u/webbrowser/SplashActivity.java
@@ -1,1 +1,22 @@
 
+package com.gbsb4u.webbrowser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Simple delay, no layout needed
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2000); // 2 seconds
+    }
+}
